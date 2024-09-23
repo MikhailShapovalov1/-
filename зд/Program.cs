3179b0e348ed65adeb61ad217bdebb340e2e1ffa
MikhailShapovalov1 <shapovalov2006mail.ru@gmail.com>
2024-09-24 02:33:57 +0600

    Add files via upload

---
+++ зд/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class задание
+{
+    static void Main()
+    {
+        int k = 5;
+        int l = 3;
+        int m = 3;
+        int n = Convert.ToInt32(Console.ReadLine());
+        int rez;
+        rez = 2 * (m + k) + 2 * n * l;
+        Console.WriteLine(rez);
+        // задачка с грядками
+    }
+}
