545cfdd3e6f83ba99a8f3e4e3002015fee3dd85b
MikhailShapovalov1 <shapovalov2006mail.ru@gmail.com>
2024-09-24 02:35:23 +0600

    Add files via upload

---
+++ зд/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a;
+        a = 10;
+        int b;
+        b = 6;
+        int max;
+        max = ((a + b) + Math.Abs(a-b))/2;
+        int min;
+        min = ((a + b) - Math.Abs(a - b))/2;
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+    }
+}
